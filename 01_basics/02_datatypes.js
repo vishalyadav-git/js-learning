b463c1dce81eab375147cd0b4fae1b0e50a29db6
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-21 11:23:12 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+// Types of DataType in JS
+
+// string
+let name = "Vishal"
+
+// number
+let age = 19
+
+// bollean 
+let isLoggedIn = true
+
+// Symbol
+// BigInt
+// null  <null is an "object">
+// undefined
+// object
+
+console.log(typeof 19);
