8aa495e40580171756e714670dc0b3ae88a11989
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-21 08:40:28 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234
+let accountName = "Vishal"
+var accountEmail = "Vishal@gmail.com"
+accountCity = "Mumbai"
+
+console.log(accountId)
+
+console.table([accountId,accountName,accountEmail,accountCity])
+
