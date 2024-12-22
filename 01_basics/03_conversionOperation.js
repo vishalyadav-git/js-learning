eaaf557bd19be7f241005d959b6e31f8045b65aa
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-22 07:56:26 +0000

    Conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,32 +1,47 @@
 // Conversion of Number into String
 let money = 89
-console.log(typeof money);
+// console.log(typeof money);
 let stringMoney = String(money)
-console.log(typeof stringMoney);
-console.log(stringMoney);
+// console.log(typeof stringMoney);
+// console.log(stringMoney);
 
 
 // Conversion of String to Number
 let name = "vishal"
-console.log(typeof name);
+// console.log(typeof name);
 let numConvert = Number(name)
-console.log(typeof numConvert);
+// console.log(typeof numConvert);
 
 // Exception of string to number
 let a = "33abc"
-console.log(typeof a);
+// console.log(typeof a);
 let b = Number(a)
-console.log(typeof b);
-console.log(b);
+// console.log(typeof b);
+// console.log(b);
 
 // conversion of boolean
  let isLoggedIn = 1
- console.log(typeof isLoggedIn);
+//  console.log(typeof isLoggedIn);
  let booleanIsloggedIn = Boolean(isLoggedIn)
- console.log(typeof booleanIsloggedIn);
- console.log(booleanIsloggedIn);
+//  console.log(typeof booleanIsloggedIn);
+//  console.log(booleanIsloggedIn);
+
+//  ************OPERATION***************
+
  
- 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let str1 = "vishal"
+let str2 = " yadav"
+
+let str3 = str1 + str2
+console.log(str3);
+
 
  
  
