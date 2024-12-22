6daa562f4074079e09a9ed46d32bf4f3a2e5db0d
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-22 07:39:16 +0000

    Conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+// Conversion of Number into String
+let money = 89
+console.log(typeof money);
+let stringMoney = String(money)
+console.log(typeof stringMoney);
+console.log(stringMoney);
+
+
+// Conversion of String to Number
+let name = "vishal"
+console.log(typeof name);
+let numConvert = Number(name)
+console.log(typeof numConvert);
+
+// Exception of string to number
+let a = "33abc"
+console.log(typeof a);
+let b = Number(a)
+console.log(typeof b);
+console.log(b);
+
+// conversion of boolean
+ let isLoggedIn = 1
+ console.log(typeof isLoggedIn);
+ let booleanIsloggedIn = Boolean(isLoggedIn)
+ console.log(typeof booleanIsloggedIn);
+ console.log(booleanIsloggedIn);
+ 
+
+ 
+ 
+ 
+
+
+
+
+
+
+
+
