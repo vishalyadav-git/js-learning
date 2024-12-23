874a27a9336aeff4c6fd7606d54b61b2a0ce00b5
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-23 07:14:05 +0000

    Summary Of Datatype

---
+++ 01_basics/Datatype-Summary.js
@@ -0,0 +1,21 @@
+// Premitive
+
+// Types: String,Number,Boolean,BigInt,Null,Undefined,Symbol
+
+let name = "vishal"
+let score = 28
+let isLoggedin = true
+let bigNumber = 23n
+let temp = "null"
+let userName;
+
+const id = Symbol(123)
+// console.log(typeof id)
+
+// NonPremitive 
+
+// Types:Array,Object,Function
+
+
+
+
