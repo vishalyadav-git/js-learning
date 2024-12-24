b67e1316cca338343d2c7f5912f061cbf40a31be
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-24 08:47:43 +0000

    Maths & Numbers

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,23 @@
+const num = 100
+// console.log(typeof num);
+
+const num1 = new Number(1000)
+// console.log(num1);
+
+// console.log(num1.toString().length);
+// console.log(typeof num1);
+
+// console.log(num1.toFixed(1));
+const num2 = 1001.234
+// console.log(num2.toPrecision(5));
+
+const num3 = 1000000
+// console.log(num3.toLocaleString("en-IN"));
+
+// ++++++++++++++++Maths++++++++++++++
+console.log(Math);
+console.log(Math.ceil(4.5)); //roundoff to bigger no.
+console.log(Math.floor(4.5)); //roundoff to smaller no.
+console.log(Math.round(4.3));
+
+
