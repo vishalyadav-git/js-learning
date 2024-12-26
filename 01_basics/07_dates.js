d1b5b4142fff5d751dea28ac6ac6e95be3420635
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-26 07:03:46 +0000

    Dates In JS

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+let dates = new Date()
+// console.log(dates);
+// console.log(dates.toString());
+// console.log(dates.toDateString());
+// console.log(dates.toLocaleDateString());
+// console.log(dates.toLocaleString("en-IN"));
+// console.log(dates.toLocaleString("en-IN"));
+
+let newDate = new Date("01-14-2023")
+console.log(newDate.toLocaleString());
+
+let timeStamp = Date.now()
+console.log(timeStamp);
+// let newDate = new Date("01-14-2023")
+console.log(newDate.getTime());
+
