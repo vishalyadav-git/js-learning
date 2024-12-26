8b140255d0b57824deec8ba6c28867c2adb8738c
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-26 13:27:47 +0000

    Basics of Array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -6,10 +6,28 @@ let heros = ["Batman", "Spiderman", "ironman", "Shaktiman"]
 // console.log(heros);
 
 heros.push("Thor")
-console.log(heros);
+// console.log(heros);
 heros.pop()
-console.log(heros);
+// console.log(heros);
 
 // Shift and Unshift
+heros.unshift("Thor")  //Pushed at "0th" index
+// console.log(heros);
+
+heros.shift()
+// console.log(heros);
+
+// slice and splice
+let arr1 = [0,1,2,3,4,5]
+console.log("A", arr1);
+
+let newArr1 = arr1.slice(1,3)  //Duplicate of array and slice a part
+console.log("B", newArr1);
+// console.log(arr1);
+let newArr2 = arr1.splice(1,3) //Manipulation in original array
+console.log("B",newArr2);
+console.log(arr1);
+
+
 
 
