55d37fe38b5d357c16ef833dcce8b8e0666e024f
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-26 13:12:28 +0000

    Basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+let arr = new Array(1,2,3,4,5)
+// console.log(arr);
+// console.log(typeof arr);
+
+let heros = ["Batman", "Spiderman", "ironman", "Shaktiman"]
+// console.log(heros);
+
+heros.push("Thor")
+console.log(heros);
+heros.pop()
+console.log(heros);
+
+// Shift and Unshift
+
+
