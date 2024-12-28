a4786075a76a4abba49792b67c29b2e0555229cb
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-28 14:19:29 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,31 @@
+const username = function(){
+// function username(){   Another Way OF Functin Defination
+
+// }
+    console.log("V");
+    console.log("I");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("L");
+}
+// username()
+// console.log(username());
+
+//Different Ways to Define a Function 
+  
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);    
+// }
+//  addTwoNumbers(3,7)
+
+function addTwoNumbers(number1, number2){
+    // const result = number1 + number2
+    // return result
+    return number1 + number2
+}
+// const result = addTwoNumbers(3,7)
+//  console.log(result);
+
+// console.log(addTwoNumbers(2,2))
+
