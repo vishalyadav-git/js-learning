0cb468bf44ff4f2313052efbbb254afa46101bb6
vishalyadav-git <yadavvishal00101@gmail.com>
2024-12-31 08:17:15 +0000

    IIFE

---
+++ 03_basics/05_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expression  (IIFE)
+// Named IIFE
+( function Tea(){
+    console.log(`DB connected`);
+    
+})();
+
+( (name) => {
+    console.log(`DB connected Welcome ${name}`);
+    
+})("Vishal")
