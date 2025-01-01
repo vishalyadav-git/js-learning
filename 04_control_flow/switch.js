fd577b8ca0fcb1045bde1e1591e13188689f219a
vishalyadav-git <yadavvishal00101@gmail.com>
2025-01-01 07:43:54 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,58 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "april"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "mar":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("default value");
+        break;
+}
+
+const day = 2
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+
+    default:
+        console.log("defualt value");
+        
+        break;
+}
