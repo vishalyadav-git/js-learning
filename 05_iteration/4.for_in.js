c14b152b07a1e749ae318339694bb603114e970e
vishalyadav-git <yadavvishal00101@gmail.com>
2025-01-03 06:57:38 +0000

    For-In loop

---
+++ 05_iteration/4.for_in.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: "JavaScript",
+    rb: "Ruby",
+    py: "python",  
+}
+
+for (const key in myObject) {
+      console.log(`${key} is shortcut for ${myObject[key]}`);
+        
+    }
+
+ const programming = ["js", "rb", "py","cpp"]   
+
+ for (const key in programming) {
+   console.log(programming[key]);
+   
+        
+ }
+ 
