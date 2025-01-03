49591d02cc3fd8d35128cfa497027cc488683ff5
vishalyadav-git <yadavvishal00101@gmail.com>
2025-01-03 07:26:39 +0000

    for_each loop

---
+++ 05_iteration/5.for_each.js
@@ -0,0 +1,38 @@
+const programming = ["ruby", "cpp", "html", "js", "java"]
+
+programming.forEach( function (item) {
+    // console.log(item);
+    
+} )
+
+// for each on arrow function
+programming.forEach( (item) => {
+    // console.log(item);
+    
+})
+
+function printIt(item){
+    // console.log(item);
+    
+}
+
+programming.forEach(printIt)
+
+const myProgram = [
+    {
+        language: "python",
+        filelang: "py",
+    },
+    {
+        language: "java",
+        filelang: "java"
+    },
+    {
+
+    },
+]
+
+myProgram.forEach( (item) => {
+    console.log(item.language);
+    
+})
